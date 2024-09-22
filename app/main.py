3f5f82aade0641d3f03075098becccc4e487f348
Vishakh Hegde <vishakhhegde948@gmail.com>
2024-09-22 13:08:01 +0530

    send correletion id

--- app/main.py
+++ app/main.py
@@ -9,20 +9,14 @@ def handle_client(client):
     client.recv(1024)
     client.sendall(create_message(7))
     client.close()
-def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     server.accept() # wait for client
 
-    while(True):
+    while True:
         client,addr=server.accept()
         handle_client(client)
-
 
 if __name__ == "__main__":
     main()
