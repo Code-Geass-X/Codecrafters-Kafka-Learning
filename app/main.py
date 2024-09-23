4bd56349019a99f169a650abc42e908e9abdb724
Vishakh Hegde <vishakhhegde948@gmail.com>
2024-09-23 08:25:07 +0530

    Binding to a port

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
 import socket  # noqa: F401
 
 
-def create_message(id):
-    id_bytes=id.to_bytes(4,byteorder="big")
-    # Message length is 8 bytes (4 bytes for the message length itself, 4 bytes for correlation ID)
-    message_length = (len(id_bytes) + 4).to_bytes(4, byteorder="big")
-    
-    # Return the full message (message length + correlation ID)
-    return message_length + id_bytes
-
-def handle_client(client):
-    client.recv(1024)
-    client.sendall(create_message(7))
-    client.close()
-
 def main():
-    server = socket.create_server(("localhost", 9092))  # Removed reuse_port=True for simplicity
-    server.listen(1)  #Ensure the server is listening for connections
+    # You can use print statements as follows for debugging,
+    # they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    # Uncomment this to pass the first stage
+    #
+    # server = socket.create_server(("localhost", 9092), reuse_port=True)
+    # server.accept() # wait for client
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+    server.accept() # wait for client
     
-    while True:
-        client, addr = server.accept()  # Accept connection
-        handle_client(client)  # Handle the connected client
-
 if __name__ == "__main__":
     main()
